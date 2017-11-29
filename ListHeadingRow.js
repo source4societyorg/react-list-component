4ebabc527033e481a2dbca1d4e0d9f48c3f855a8
Daniel Fredriksen <dfredriksen@nractech.com>
2017-11-29 06:11:29 +0000

    ESLint corrections, added keys to header rows

---
+++ ListHeadingRow.js
@@ -0,0 +1,4 @@
+import styled from 'styled-components';
+
+const ListHeadingRow = styled.tr``;
+export default ListHeadingRow;
--- index.js
+++ index.js
@@ -1,55 +1,50 @@
 import React from 'react';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import ListHeadings from './ListHeadings';
 import ListHeading from './ListHeading';
+import ListHeadingRow from './ListHeadingRow';
 import ListBody from './ListBody';
 import ListContainer from './ListContainer';
 
 const renderListHeadings = (headings) => (
- <ListHeadings>
-   {renderListHeading(headings)}
- </ListHeadings>
-);   
+  <ListHeadings>
+    <ListHeadingRow>
+      {renderListHeading(headings)}
+    </ListHeadingRow>
+  </ListHeadings>
+);
 
 const renderListHeading = (headings) => (
- headings.map( (heading) => (    
-    <ListHeading sortKey={heading.sortKey} isSorting={heading.isSorting} sortAscending={heading.sortAscending}>{heading.title}</ListHeading>
+ headings.map((heading, index) => (
+   <ListHeading key={`${heading.title}_${index}`} sortKey={heading.sortKey} isSorting={heading.isSorting} sortAscending={heading.sortAscending}>{heading.title}</ListHeading> // eslint-disable-line react/no-array-index-key
  ))
 );
 
 const renderListBody = (children) => (
-  children.map( (index) => (
-    <ListBody>
-      {renderListBodyCells(children)}
-    </ListBody>
-  ))
-);
-
-const renderListBodyCells = (children) => (
-  children.map( (child, index) => child )
+  <ListBody>
+    {children}
+  </ListBody>
 );
 
 const List = (props) => (
-    <ListContainer>
-        {renderListHeadings(props.headings)}  
-        {renderListBody(props.children)}
-    </ListContainer>
+  <ListContainer>
+    {renderListHeadings(props.headings)}
+    {renderListBody(props.children)}
+  </ListContainer>
 );
 
 List.defaultProps = {
-    headings: [],
-    items: [],
-}
+  headings: [],
+};
 
 List.propTypes = {
-    headings: PropTypes.arrayOf(PropTypes.shape({
-        title: PropTypes.string,
-        sortKey: PropTypes.string.isRequired,
-        isSorting: PropTypes.bool.isRequired,
-        sortAscending: PropTypes.bool.isRequired
-    })),
-    items: PropTypes.array,
-}
+  headings: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    sortKey: PropTypes.string.isRequired,
+    isSorting: PropTypes.bool.isRequired,
+    sortAscending: PropTypes.bool.isRequired,
+  })),
+  children: PropTypes.node,
+};
 
 export default List;
